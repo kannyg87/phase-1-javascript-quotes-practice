3df46254a49b673317865813f89138753dab348e
Kanny <kanny.ghafour87@gmail.com>
2023-08-09 17:50:12 -0500

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-const li = document.createElement('li')
-const blockquote = document.createElement('blockquote')
-const btnLike = document.createElement('button')
-const btnDel = document.createElement('button')
-
-console.log((btnDel))
 fetch('http://localhost:3000/quotes?_embed=likes')
-.then(res=>res.json())
-.then(data=>{
-  console.log(data)
-  data.forEach(element => {
-   blockquote.innerHTML +=`<p class="mb-0">${element.quote}.</p>
-   <footer class="blockquote-footer">${element.author}</footer>
-   <br>
-   <button class='btn-success'>Likes: <span></span></button>`
-   const ul =document.getElementById("quote-list")
-   li.appendChild(blockquote)
-   ul.appendChild(li)
-   const likeBtn = blockquote.querySelector('.btn-success')
-   console.log(likeBtn)
+  .then(res => res.json())
+  .then(data => {
+    const ul = document.getElementById("quote-list");
+    data.forEach(element => {
+      const li = document.createElement('li');
+      const blockquote = document.createElement('blockquote');
+      const btnLike = document.createElement('button');
+      const btnDel = document.createElement('button');
 
-   likeBtn.addEventListener('click', function(){
-    console.log("object")
+      blockquote.innerHTML += `<p class="mb-0">${element.quote}.</p>
+      <footer class="blockquote-footer">${element.author}</footer>
+      <br>
+      <button class='btn-success'>Likes: <span></span></button>`;
+
+      li.appendChild(blockquote);
+      ul.appendChild(li)
+      const newLike = {
+        quoteId: element.id,
+        createdAt: Date.now(), 
+      };
+      
+      // dataObject.likes.push(newLike);
+      const likeBtn = blockquote.querySelector('.btn-success');
+      likeBtn.addEventListener('click', function () {
+        fetch(`http://localhost:3000/likes`,{
+          method:'POST',
+          headers: {
+            'Content-Type': "application/json",
+          },
+          body: JSON.stringify(newLike)
+      })
+      .then(response =>response.json())
+      .then(data =>console.log(data))
+    });
   })
-
-  });
 })
